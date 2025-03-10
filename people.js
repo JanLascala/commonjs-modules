060a5901a21035354ac759e3122f81cad2fc7dd1
JanLascala <lascala.jan@gmail.com>
2025-03-10 14:28:54 +0100

    Exe complete

--- people.js
+++ people.js
@@ -1,2 +1,17 @@
 const fullName = require(`./names`)
 const hobbies = require(`./hobbies`)
+
+
+function people() {
+    const fullNames = fullName("john", "dhoe")
+    const hobbie = hobbies("archery")
+
+    return {
+        fullNames,
+        hobbie
+    }
+}
+
+const john = people()
+
+console.log(john)
